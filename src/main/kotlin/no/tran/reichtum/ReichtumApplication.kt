5bdd50dd22faf5e7edac7bfb213e862de8ee030a
Tommy Tran <tommytritran@gmail.com>
2022-01-26 14:22:28 +0100

    first commit

---
+++ src/main/kotlin/no/tran/reichtum/ReichtumApplication.kt
@@ -0,0 +1,11 @@
+package no.tran.reichtum
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReichtumApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReichtumApplication>(*args)
+}
---
+++ src/test/kotlin/no/tran/reichtum/ReichtumApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.tran.reichtum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReichtumApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
